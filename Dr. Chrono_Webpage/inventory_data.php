2278d064d9641958b021b88d5f93d5d3f659c8dd
Arnav Mahajan <arnav.mahajan.2001@gmail.com>
2021-01-16 18:19:38 +0530

    Web page without chatbot

--- Dr. Chrono_Webpage/inventory_data.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $inventory_data = '{"country":["India","Italy","UK","Germany","China"], "ppe_available_qty":[{"India":13000}, {"Italy":12000}, {"UK":8110}, {"Germany":9130}, {"China": 17000}], "ppe_order_qty":[{"India":25000}, {"Italy":5600}, {"UK":2500}, {"Germany":10000}, {"China":20000}], "ppe_avg_cost_GBP":[{"India":2.59}, {"Italy":10.68}, {"UK":3.76}, {"Germany":7.84}, {"China":7.58}], "ppe_avg_delivery_time_days":[{"India":21}, {"Italy":6}, {"UK":3.33}, {"Germany":3}, {"China":20}]}';
-
-    echo $inventory_data;
-
-?>
