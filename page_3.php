65ae2b1acf5837ef9ea2f253c93b41eca22b0bcd
ayugupt <ayush.gupta.10301@gmail.com>
2021-01-17 00:37:25 +0530

    added chatbot

--- page_3.php
+++ page_3.php
@@ -51,6 +51,7 @@
 
   
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -62,11 +63,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <!-- custom css -->
     <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="assets/css/chatbot.css">
     
     <title>Medi Chain!</title>
   </head>
   <body>
-  
+    <div id="mySidebar" class="sidebar">
+        <a class="closebtn" onclick="closeNav()">×</a>
+        <h2 style="text-align: center; color: #f1f1f1;">Chatbot</h2>
+        <div class="chatLog" style="flex:0.95;">
+             <ul id="chatList">
+             </ul>
+        </div>
+        <div class="inputChat">
+            <form id="chatForm" method="GET"> 
+              <input type="submit" id="sendToChatbot" style="float: left" value="Send"/>
+              <input type="text" id="mssgToChatbot" style="float: right; width: 170px;" name="message"/>
+            </form>
+        </div>
+      </div>
+    <div id="main">
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
@@ -83,7 +99,7 @@
                             <a href="dashboard.html" style="text-decoration: none; color:white;">Supply Chain</a>
                             </li>
                             <li class="main_navbar_item" id="main_navbar_item">
-                               <a href="#" style="text-decoration: none; color:white;"></a> Chatbot
+                               <a href="#" style="text-decoration: none; color:white;" onclick="openNav()">Chatbot</a> 
                             </li>
                         </ul>
                     </div>
@@ -170,10 +186,20 @@
             </div>
         </div>
     </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="data/httpRequest.js"></script>
     <script>
+        function openNav() {
+  
+            document.getElementById("mySidebar").style.width = "250px";
+            }
+
+        function closeNav() {
+            document.getElementById("mySidebar").style.width = "0";
+            }
         const form = document.forms[0]
         $(document).ready(function(){
             getAppointmentData(1)
